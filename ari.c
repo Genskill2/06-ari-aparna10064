90104a4e9aef25a1a6dc9bd876da98808397eb75
aparna10064 <72874041+aparna10064@users.noreply.github.com>
2021-06-30 21:01:02 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -30,32 +30,32 @@ string ari(string s)
  
 switch(ari_total){
  case 1:
-  return " "Kindergarten";
-    case 2:
-        return "First/Second Grade";
-    case 3:
-        return "Third Grade";
-    case 4:
-        return "Fourth Grade";
-    case 5:
-        return "Fifth Grade";
-    case 6:
-        return "Sixth Grade";
-    case 7:
-        return "Seventh Grade";
-    case 8:
-        return "Eight Grade";
-    case 9:
-        return "Ninth Grade";
-    case 10:
-        return "Tenth Grade";
-    case 11:
-        return "Eleventh Grade";
-    case 12:
-        return "Twelfth Grade";
-    case 13:
-        return "College student";
-    case 14:
-        return "Professor";
+  return "Kindergarten";
+ case 2:
+  return "First/Second Grade";
+ case 3:
+  return "Third Grade";
+ case 4:
+  return "Fourth Grade";
+ case 5:
+  return "Fifth Grade";
+ case 6:
+  return "Sixth Grade";
+ case 7:
+  return "Seventh Grade";
+ case 8:
+  return "Eight Grade";
+ case 9:
+  return "Ninth Grade";
+ case 10:
+  return "Tenth Grade";
+ case 11:
+  return "Eleventh Grade";
+ case 12:
+  return "Twelfth Grade";
+ case 13:
+  return "College student";
+ case 14:
+  return "Professor";
     }
 }
